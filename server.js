225075bd8587bb60b0fcd466a59243e58bf2e2b0
FehSchiavon <felipschiavon@gmail.com>
2020-09-23 22:25:14 -0300

    init project data

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-// Aprendendo mais de dados
-// Instalando PostgreSQL
-// tar.gz  and run
-
-// Try to install 
-// Try find solution Postbird
-
-// Read for all post Rocketseat
-// Try unintall to next intall 
-
-// Post to Forum
+// Agora foi Postgres e Posrbird
