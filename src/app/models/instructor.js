861fe088341cc1d86ae3f4357a8a34f811b89ca8
FehSchiavon <felipschiavon@gmail.com>
2020-10-05 06:41:13 -0300

    models / instructor / callback function

---
+++ src/app/models/instructor.js
@@ -0,0 +1,11 @@
+module.exports = {
+    all(callback) {
+        db.query(`SELECT * FROM instructors`, function(err, results) {
+            if(err) return res.send('Database Error!')
+            callback(results.rows)
+        })
+    },
+    create() {
+
+    }
+}
