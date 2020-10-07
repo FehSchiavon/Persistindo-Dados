92dac3441b04b4ceaf8e54b1b8b5b0b52bcee03f
FehSchiavon <felipschiavon@gmail.com>
2020-10-07 06:27:01 -0300

    create find instructors

--- src/app/models/instructor.js
+++ src/app/models/instructor.js
@@ -36,5 +36,13 @@ module.exports = {
 
             callback(results.rows[0])
         })
+    },
+    find(id, callback) {
+        db.query(`SELECT * 
+        FROM instructors 
+        WHERE id = $1`, [id], function(err, results) {
+            if(err) return res.send("Database Error!")
+            callback(results.rows[0])
+        })
     }
 }
