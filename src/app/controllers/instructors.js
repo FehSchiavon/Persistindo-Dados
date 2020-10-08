f836595d20117822aa43a104066379406dec6a06
FehSchiavon <felipschiavon@gmail.com>
2020-10-08 06:38:18 -0300

    edit ok

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -34,9 +34,17 @@ module.exports = {
 
             return res.render('instructors/show', { instructor })
         })
+
     },
     edit(req, res) {
-        return
+        Instructor.find(req.params.id, function(instructor) {
+            if (!instructor) return res.send('Instructor not found!')
+
+            instructor.birth = date(instructor.birth).iso
+
+            return res.render('instructors/edit', { instructor })
+        })
+
     },
     put(req, res) {
         const keys = Object.keys(req.body)
