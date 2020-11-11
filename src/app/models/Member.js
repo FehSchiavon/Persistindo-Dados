b5135dfe49bbb38b12e83531f2e39772a245abf1
FehSchiavon <felipschiavon@gmail.com>
2020-11-11 06:49:33 -0300

    consume data member, but add update

--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -21,7 +21,7 @@ module.exports = {
                 weight,
                 height,
                 instructor_id
-            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
             RETURNING id
         `
 
@@ -33,7 +33,8 @@ module.exports = {
             date(data.birth).iso,
             data.blood,
             data.weight,
-            data.height
+            data.height,
+            data.instructor
         ]
         
         db.query(query, values, function(err, results) {
@@ -59,8 +60,9 @@ module.exports = {
             email=($5),
             blood=($6),
             weight=($7),
-            height=($8)
-        WHERE id = ($9)
+            height=($8),
+            instructor_id=($9)
+        WHERE id = ($10)
         `
 
         const values = [
