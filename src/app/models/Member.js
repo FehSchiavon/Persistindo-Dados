07c4287fed18998feab36eebdbb620dcaa09bbc6
FehSchiavon <felipschiavon@gmail.com>
2020-11-11 06:46:53 -0300

    solver bug create members

--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -19,7 +19,8 @@ module.exports = {
                 birth,
                 blood,
                 weight,
-                height
+                height,
+                instructor_id
             ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
             RETURNING id
         `
