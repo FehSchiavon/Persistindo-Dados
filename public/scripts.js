e22179cbe714486ac2aa36448f0e041d78e316de
FehSchiavon <felipschiavon@gmail.com>
2020-12-22 09:25:24 -0300

    Separando filter Intructor / Members

--- public/scripts.js
+++ public/scripts.js
@@ -35,25 +35,33 @@ function paginate(selectedPage, totalPages) {
     console.log(pages) // Dados do Array
 }
 
-const pagination = document.querySelector(".pagination")
-const filter = pagination.dataset.filter
-const page = +pagination.dataset.page
-const total = +pagination.dataset.total
-const pages = paginate(page, total)
+function createPagination(pagination) {
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+    const pages = paginate(page, total)
 
-let elements = ""
+    let elements = ""
 
-for (let page of pages) {
-    if(String(page).includes("...")) {
-        elements += `<span>${page}</span>`
-    } else {
-        if (filter) {
-            elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+    for (let page of pages) {
+        if(String(page).includes("...")) {
+            elements += `<span>${page}</span>`
         } else {
-            elements += `<a href="?page=${page}">${page}</a>`
-        }
+            if (filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            } else {
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
 
+        }
     }
+
+    pagination.innerHTML = elements
 }
 
-pagination.innerHTML = elements
+const pagination = document.querySelector(".pagination")
+
+if (pagination) {
+    
+}
+
